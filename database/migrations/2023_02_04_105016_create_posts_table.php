dcd69358f5fbc8a9743b25103c9ce073d00107bc
AkrmEdm <akrmedm@gmail.com>
2023-02-04 18:15:57 +0500

    - update

--- database/migrations/2023_02_04_105016_create_posts_table.php
+++ database/migrations/2023_02_04_105016_create_posts_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use ProtoneMedia\Splade\Facades\SEO;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -41,6 +43,9 @@ Route::middleware('splade')->group(function () {
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
+
+    Route::resource('categories', CategoryController::class);
+    Route::resource('posts', PostController::class);
 
     require __DIR__.'/auth.php';
 });
